3ac5673c0773103dfb871ddd4288e795a44a0621
JoshuaG353 <joshuagonzalezmacin@gmail.com>
2025-12-03 19:44:27 -0600

    feat: environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 
 export const environment = {
   production: false,
-  url_api: "http://127.0.0.1:8000"
+  //url_api: "http://127.0.0.1:8000"
+  url_api: "https://app-movil-escolar-api-9x8i.onrender.com"
 };
